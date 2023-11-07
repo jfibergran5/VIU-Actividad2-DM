21e2f4e81fb03ebd3b041a50fdd0d74108cdf2b9
Jaime F <jfibergran@gmail.com>
2023-11-08 00:06:51 +0100

    Nada

--- App.js
+++ App.js
@@ -4,7 +4,19 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+
       <Text>¡ Iniciando proyecto !</Text>
+
+      <Text>¡ View !</Text>
+      <Text>¡ SafeAreaView !</Text>
+      <Text>¡ Text !</Text>
+      <Text>¡ Image !</Text>
+      <Text>¡ TouchableOpacity !</Text>
+      <text>ScrollView</text>
+      <text>FlatList</text>
+
+
+
       <StatusBar style="auto" />
     </View>
   );
